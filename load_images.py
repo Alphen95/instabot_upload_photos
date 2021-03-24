3cec696e3f4a9878bcf1001562161ea29a847400
Alphen95 <57586762+Alphen95@users.noreply.github.com>
2021-03-24 20:52:17 +0500

    Update load_images.py

--- load_images.py
+++ load_images.py
@@ -2,13 +2,14 @@ from PIL import Image
 
 import requests
 import os
+import urllib
 
 def load_image(filepath, links):
     for link_id in range(len(links)):
         link = links[link_id]
         response = requests.get(link)
         response.raise_for_status()
-        with open(filepath[:-5] + str(link_id) + filepath[-5:], 'wb') as file:
+        with open(os.path.splitext(filepath)[0] + str(link_id) + os.path.splitext(filepath)[1], 'wb') as file:
             file.write(response.content)
 
 
@@ -26,35 +27,34 @@ def fetch_hubble_telescope_images(img_id):
     response.raise_for_status()
     full_response = response.json()
     links = []
-    if type(full_response["image_files"]) != "str":
+    if not(isinstance(full_response["image_files"], str)):
         for image_params in full_response["image_files"]:
             links.append("https:" + image_params['file_url'])
     else:
         links = "https:" + image_params['file_url']
-    return(links)
+    return links
 
 
 def split_links_to_filenames(img_id, links):
     filenames = []
     for link in links:
-        filenames.append(str(img_id) + "_" + link.split("/")[-1])
+        path = urllib.parse.unquote(os.path.split(urllib.parse.urlsplit(link)[2])[1])
+        filenames.append(str(img_id) + "_" + path)
     return filenames
 
 
 def resize_image(filepath):
     image = Image.open(filepath)
     image.thumbnail((1080, 768))
-    print(image.size)
     image.convert('RGB')
-    image.save(filepath.split(".")[0] + ".jpg")
+    image.save(os.path.splitext(filepath)[0] + ".jpg")
 
 
 def download_hublle_telescope_images(img_id):
     links = fetch_hubble_telescope_images(img_id)
     filenames = split_links_to_filenames(img_id, links)
     for link_id in range(len(links)):
-        link = links[link_id]
-        response = requests.get(link, verify=False)
+        response = requests.get(links[link_id], verify=False)
         with open("images/" + filenames[link_id], 'wb') as file:
             file.write(response.content)
 
@@ -78,7 +78,6 @@ if __name__ == "__main__":
     files = os.listdir(str(os.getcwd()).replace("\\", "/") + "/images")
     for img_file in files:
         try:
-            print(img_file)
             resize_image(str(os.getcwd()).replace("\\", "/") + "/images/" + img_file)
         except:
             pass
