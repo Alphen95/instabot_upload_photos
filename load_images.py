a62b7ac4b62a586b792661ef875d25fe774367e9
Alphen95 <alphen@MacBook-Air-alphen.local>
2021-04-07 21:08:54 +0500

    Update load_images.py

--- load_images.py
+++ load_images.py
@@ -26,7 +26,16 @@ def fetch_hubble_telescope_images(img_id):
     response = requests.get(hubble_site_url)
     response.raise_for_status()
     full_response = response.json()
+<<<<<<< HEAD
     links = "https:{}".format( image_params['file_url'])
+=======
+    links = []
+    if not(isinstance(full_response["image_files"], str)):
+        for image_params in full_response["image_files"]:
+            links.append("https:{}".format( image_params['file_url']))
+    else:
+        links = "https:{}".format( image_params['file_url'])
+>>>>>>> parent of 1fd8dd8 (Update load_images.py)
     return links
 
 
