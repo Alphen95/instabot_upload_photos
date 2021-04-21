f835ef7b1de3959dd24696fee0034c93d2eb77e7
Alphen95 <57586762+Alphen95@users.noreply.github.com>
2021-04-21 21:14:41 +0500

    some atomary commits magically merged into one
    added:
    -load_images.py:
    --additional variables for easier undertandig of code
    --spaces between some commas
    
    moved:
    -auto_load.py:
    --the whole block of code to the ifmain
    
    -load_images.py:
    --some variables
    
    changed:
    -load_images.py
    --windows slashes support to normal

--- auto_load.py
+++ auto_load.py
@@ -6,68 +6,72 @@ import pathlib
 from io import open
 from dotenv import load_dotenv
 import argparse
-load_dotenv()
-username_instabot = os.getenv("USERNAME_INSTABOT")
-password_instabot = str(os.getenv("PASSWORD_INSTABOT"))
+from instabot import Bot
 
-os.chdir(str(pathlib.Path(__file__).parent.absolute()).replace("\\", "/"))
-from instabot import Bot  # noqa: E402
+def join_image_path(pic_filename):
+    pic_name = pic_filename[:-4].split("-")
+    return "-".join(pic_name[1:])    
 
-posted_pic_list = []
-try:
-    with open("pics.txt", "r", encoding="utf8") as f:
-        posted_pic_list = f.read().splitlines()
-except Exception:
+def make_bot(username,password):
+    bot = Bot()
+    bot.login(username=username, password=password, use_cookie=False)    
+
+if __name__ == "__main__":
+    load_dotenv()
+    username_instabot = os.getenv("USERNAME_INSTABOT")
+    password_instabot = str(os.getenv("PASSWORD_INSTABOT"))
+    
+    os.chdir(str(pathlib.Path(__file__).parent.absolute()).replace("\\", "/"))
     posted_pic_list = []
-minutes_timeout = 30
-
-bot = Bot()
-bot.login(username=username_instabot, password=password_instabot, use_cookie=False)
-
-parser = argparse.ArgumentParser(
-    description='автоматически загружаем фоточки в инстаграм'
-)
-parser.add_argument('--minutes', help='задержка в минутах')
-args = parser.parse_args()
-if args.minutes != None: minutes_timeout = args.minutes
-
-timeout = minutes_timeout * 60
-
-while True:
-    folder_path = "./images"
-    pics = glob.glob(folder_path + "/*.jpg")
-    pics = sorted(pics)
     try:
-        for pic in pics:
-            if pic in posted_pic_list:
-                continue
-
-            pic_name = pic[:-4].split("-")
-            pic_name = "-".join(pic_name[1:])
-
-            print("upload: " + pic_name)
-
-            description_file = folder_path + "/" + pic_name + ".txt"
-
-            if os.path.isfile(description_file):
-                with open(description_file, "r") as file:
-                    caption = file.read()
-            else:
-                caption = pic_name.replace("-", " ")
-
-            bot.upload_photo(pic, caption=caption)
-            if bot.api.last_response.status_code != 200:
-                print(bot.api.last_response)
-                # snd msg
-                break
-
-            if pic not in posted_pic_list:
-                posted_pic_list.append(pic)
-                with open("pics.txt", "a", encoding="utf8") as f:
-                    f.write(pic + "\n")
-
-            time.sleep(timeout)
-
-    except Exception as e:
-        print(str(e))
-    time.sleep(60)
+        with open("pics.txt", "r", encoding="utf8") as f:
+            posted_pic_list = f.read().splitlines()
+    except Exception:
+        posted_pic_list = []
+    minutes_timeout = 30
+    
+    parser = argparse.ArgumentParser(
+        description='автоматически загружаем фоточки в инстаграм'
+    )
+    parser.add_argument('--minutes', help='задержка в минутах')
+    args = parser.parse_args()
+    if args.minutes != None: minutes_timeout = args.minutes
+    
+    timeout = minutes_timeout * 60
+    
+    
+    while True:
+        folder_path = "./images"
+        pics = glob.glob(folder_path + "/*.jpg")
+        pics = sorted(pics)
+        try:
+            for pic in pics:
+                if pic in posted_pic_list:
+                    continue
+    
+                
+    
+                print("upload: " + pic_name)
+    
+                description_file = folder_path + "/" + pic_name + ".txt"
+    
+                if os.path.isfile(description_file):
+                    with open(description_file, "r") as file:
+                        caption = file.read()
+                else:
+                    caption = pic_name.replace("-", " ")
+    
+                bot.upload_photo(pic, caption=caption)
+                if bot.api.last_response.status_code != 200:
+                    break
+    
+                if pic not in posted_pic_list:
+                    posted_pic_list.append(pic)
+                    with open("pics.txt", "a", encoding="utf8") as f:
+                        f.write(pic + "\n")
+    
+                time.sleep(timeout)
+    
+        except Exception as e:
+            print(str(e))
+        time.sleep(60)
--- load_images.py
+++ load_images.py
@@ -6,14 +6,16 @@ import urllib
 import pathlib
 import platform
 
-folder_name = "images"
 
 def load_image(filepath, links):
-    for link_id in range(len(links)):
+    for link_id, link in enumerate(links):
         link = links[link_id]
         response = requests.get(link)
         response.raise_for_status()
-        with open("{0}{1}{2}".format(os.path.splitext(filepath)[0], str(link_id), os.path.splitext(filepath)[1]), 'wb') as file:
+        if os.path.splitext(filepath)[1] != "":ext = os.path.splitext(filepath)[1]
+        else: ext = ".jpg"
+
+        with open("{0}{1}{2}".format(os.path.splitext(filepath)[0], str(link_id), ext), "wb") as file:
             file.write(response.content)
 
 
@@ -22,7 +24,7 @@ def fetch_spacex_last_launch(folder_name="images"):
     response = requests.get(spacexdata_url)
     response.raise_for_status()
     full_response = response.json()
-    path = pathlib.PureWindowsPath("{0}/{1}".format(folder_name,filenames[link_id])) if platform.system == "Windows" else "{0}/{1}".format(folder_name,"image.jpeg")
+    path = os.path.join(folder_name,"")
     load_image(path.format(folder_name), full_response["links"]["flickr"]["original"])
 
 
@@ -31,12 +33,11 @@ def fetch_hubble_telescope_images(img_id):
     response = requests.get(hubble_site_url)
     response.raise_for_status()
     full_response = response.json()
-    print(full_response)
     links = []
     if not(isinstance(full_response["image_files"], str)):
-        links.append("https:{}".format( full_response["image_files"][0]['file_url']))
+        links.append("https:{}".format( full_response["image_files"][0]["file_url"]))
     else:
-        links = "https:{}".format( full_response['file_url'])
+        links = "https:{}".format( full_response["file_url"])
     return links
 
 
@@ -50,39 +51,41 @@ def split_links_to_filenames(img_id, links):
 
 def resize_image(filepath):
     image = Image.open(filepath)
-    image.thumbnail((1080, 768))
-    image.convert('RGB')
+    img_size = (1080, 768)
+    image.thumbnail(img_file)
+    image.convert("RGB")
     image.save("{}.jpg".format(os.path.splitext(filepath)[0]))
 
 
-def download_hublle_telescope_images(img_id,folder_name="images"):
+def download_hublle_telescope_images(img_id, folder_name="images"):
     links = fetch_hubble_telescope_images(img_id)
     filenames = split_links_to_filenames(img_id, links)
-    for link_id in range(len(links)):
-        response = requests.get(links[link_id], verify=False)
-        path = pathlib.PureWindowsPath("{0}/{1}".format(folder_name,filenames[link_id])) if platform.system == "Windows" else "{0}/{1}".format(folder_name,filenames[link_id])
-        with open(path, 'wb') as file:
+    for link_id, link in enumerate(links):
+        response = requests.get(link, verify=False)
+        path = os.path.join(folder_name,filenames[link_id])
+        with open(path, "wb") as file:
             file.write(response.content)
 
 
-def download_hubble_telescope_images_by_collection_name(collection_name,folder_name="imgaes"):
+def download_hubble_telescope_images_by_collection_name(collection_name, folder_name="imgaes"):
     hubble_site_url = "http://hubblesite.org/api/v3/images/{}".format(collection_name)
     response = requests.get(hubble_site_url)
     response.raise_for_status()
     full_response = response.json()
     image_ids = []
     for image_params in full_response:
-        download_hublle_telescope_images(image_params['id'],folder_name)
+        download_hublle_telescope_images(image_params["id"], folder_name)
 
 
 if __name__ == "__main__":
-    try:os.makedirs(folder_name)
-    except:pass
+    folder_name = "images"
+    os.makedirs(folder_name, exist_ok=True)
 
     fetch_spacex_last_launch(folder_name)
-    download_hubble_telescope_images_by_collection_name("news",folder_name)
-    files = os.listdir("{0}{1}".format(str(pathlib.Path(__file__).parent.absolute()), folder_name))
+    download_hubble_telescope_images_by_collection_name("news", folder_name)
+    files = os.listdir("{0}/{1}".format(str(pathlib.Path(__file__).parent.absolute()), folder_name))
     for img_file in files:
         try:
-            resize_image(pathlib.PureWindowsPath("{0}/{1}/{2}".format(str(pathlib.Path(__file__).parent.absolute()),folder_name,img_file)) if platform.system == "Windows" else "{0}/{1}/{2}".format(str(pathlib.Path(__file__).parent.absolute()),folder_name,img_file))
-        except:pass
+            resize_image(os.path.join(str(pathlib.Path(__file__).parent.absolute()), folder_name,img_file))
+        except:
+            pass
